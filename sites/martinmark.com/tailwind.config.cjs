8527cb9c7bf0e4ce3d1aeebd07eb3130f394538d
Martin Mark <martin@martinmark.com>
2022-09-05 15:23:42 -0400

    Updated site for Astro => changed to main / removed AppSmith

---
+++ sites/martinmark.com/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+				mono: ['Overpass Mono', 'ui-sans-serif', 'system-ui'],
+			},
+			listStyleType: {
+				square: 'square'
+			}	
+		},
+	},
+	plugins: [],
+}
